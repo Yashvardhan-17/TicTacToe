2f13fcccb1029acd04b63a37c96e6a00a3dcbb64
Yashvardhan-17 <yashvardhandiwan@gmail.com>
2024-09-24 14:57:30 +0530

    added the title to the Ui

--- app/src/main/java/com/example/tictactoe/view/BoardUI.kt
+++ app/src/main/java/com/example/tictactoe/view/BoardUI.kt
@@ -1,5 +1,6 @@
 package com.example.tictactoe.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,10 +9,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,12 +26,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tictactoe.Logic.checkWinner
+import com.example.tictactoe.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TicTacToeGame(){
 
@@ -34,62 +44,70 @@ fun TicTacToeGame(){
     var winner by remember { mutableStateOf<String?>(null)  }
 
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        
-        Text(text = "Tic Tac Toe", fontSize = 30.sp, fontWeight = FontWeight.Bold)
-        
-        Spacer(modifier = Modifier.height(50.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        for(i in 0..2){
-            Row {
-                for (j in 0..2){
-                    Box(
-                        modifier = Modifier
-                            .size(100.dp)
-                            .background(Color.LightGray)
-                            .clickable(enabled = boardState[i][j].isEmpty() && winner == null) {
-                                if (boardState[i][j].isEmpty()) {
-                                    val newBoardState = boardState
-                                        .map { it.toMutableList() }
-                                        .toMutableList()
-                                    newBoardState[i][j] = currentPlayer
-                                    boardState = newBoardState
+            Image(
+                painter = painterResource(id = R.drawable.ttt),
+                contentDescription ="ttt",
+                modifier = Modifier
+                    .height(100.dp)
+                    .fillMaxWidth()
+                    .padding(top=50.dp)
+            )
+            Column(
+                modifier = Modifier.padding(top=125.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                for(i in 0..2){
+                    Row {
+                        for (j in 0..2){
+                            Box(
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .background(Color.LightGray)
+                                    .clickable(enabled = boardState[i][j].isEmpty() && winner == null) {
+                                        if (boardState[i][j].isEmpty()) {
+                                            val newBoardState = boardState
+                                                .map { it.toMutableList() }
+                                                .toMutableList()
+                                            newBoardState[i][j] = currentPlayer
+                                            boardState = newBoardState
 
-                                    currentPlayer = if (currentPlayer == "X") "O" else "X"
-                                    winner = checkWinner(boardState)
-                                }
-                            },
-                        contentAlignment = Alignment.Center
-                    ){
-                        Text(text = boardState[i][j], fontSize = 36.sp)
+                                            currentPlayer = if (currentPlayer == "X") "O" else "X"
+                                            winner = checkWinner(boardState)
+                                        }
+                                    },
+                                contentAlignment = Alignment.Center
+                            ){
+                                Text(text = boardState[i][j], fontSize = 36.sp)
+                            }
+                        }
                     }
+                }
+
+                if(winner!=null){
+                    Text(text = "Winner:$winner", fontSize = 24.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top=20.dp))
+                }else if(boardState.flatten().none(){it.isEmpty()}){
+                    Text(text = "It's Draw ", fontSize = 24.sp, fontWeight = FontWeight.Bold,modifier = Modifier.padding(top=20.dp))
+                }
+
+                Spacer(modifier = Modifier.height(50.dp))
+
+                Button(
+                    onClick = {
+                        boardState = List(3){ MutableList(3){""} }
+                        winner=null
+                        currentPlayer="X"
+                    }) {
+                    Text(text = "Restart")
                 }
             }
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        if(winner!=null){
-            Text(text = "Winner:$winner", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        }else if(boardState.flatten().none(){it.isEmpty()}){
-            Text(text = "It's Draw ", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        }
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Button(
-            onClick = {
-            boardState = List(3){ MutableList(3){""} }
-            winner=null
-            currentPlayer="X"
-        }) {
-            Text(text = "Restart")
-        }
-    }
 }
 
 @Preview
